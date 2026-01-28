578cdd2134cd8102000d19fe224cbe24e8289bd6
Gracious <graciousometere@gmail.com>
2026-01-28 01:14:59 +0100

    factory to create post from json

--- post.dart
+++ post.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class Post {
   final int? id;
   final int userId;
@@ -10,4 +14,21 @@ class Post {
     required this.title,
     required this.body,
   });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+  
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'title': title,
+      'body': body,
+    };
+  }
 }
